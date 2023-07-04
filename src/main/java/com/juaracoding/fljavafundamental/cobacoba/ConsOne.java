02cddd27614d63f909946a03e6fbee7dab696282
fajarseptian <fauziahlatifah13@gmail.com>
2023-07-04 20:09:43 +0700

    commit

---
+++ src/main/java/com/juaracoding/fljavafundamental/cobacoba/ConsOne.java
@@ -0,0 +1,33 @@
+package com.juaracoding.fljavafundamental.cobacoba;
+/*
+IntelliJ IDEA 2023.1.2 (Community Edition)
+Build #IC-231.9011.34, built on May 16, 2023
+@Author Asus a.k.a. Fauziah Latifah
+Java Developer
+Created on 7/4/2023 8:01 PM
+@Last Modified 7/4/2023 8:01 PM
+Version 1.0
+*/
+
+public class ConsOne {
+
+    private String strKata;
+    private int intAngka;
+
+    public ConsOne(){
+        strKata = "Hello";
+    }
+
+    public ConsOne(String strKata){
+        this.strKata = strKata;
+    }
+
+    public String getStrKata() {
+        return strKata;
+    }
+
+    public void setStrKata(String strKata) {
+        this.strKata = strKata;
+    }
+
+}
---
+++ src/main/java/com/juaracoding/fljavafundamental/cobacoba/ConsTwo.java
@@ -0,0 +1,20 @@
+package com.juaracoding.fljavafundamental.cobacoba;
+/*
+IntelliJ IDEA 2023.1.2 (Community Edition)
+Build #IC-231.9011.34, built on May 16, 2023
+@Author Asus a.k.a. Fauziah Latifah
+Java Developer
+Created on 7/4/2023 8:01 PM
+@Last Modified 7/4/2023 8:01 PM
+Version 1.0
+*/
+
+public class ConsTwo {
+    public static void main(String[] args) {
+        ConsOne consOne = new ConsOne();
+        System.out.println(consOne.getStrKata());
+
+        consOne = new ConsOne("Hai");
+        System.out.println(consOne.getStrKata());
+    }
+}
